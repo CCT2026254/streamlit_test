d1f734116c595094834dadca88c86d037ece47b0
marikotvanova <2026254@student.cct.ie>
2026-05-18 12:31:55 +0100

    Update uber.py

--- uber.py
+++ uber.py
@@ -18,4 +18,4 @@ def load_data(nrows):
 
 data_load_state = st.text("Loading Data ...")
 load_data(10000)
-data_load_state = st.text("Loading Data ... Done!")
+data_load_state = st.text("Loading Data ... Done! 🥳")
